763d2b6a5af113481d611100e44e33ca0066e6f8
prhineh1 <rhinehartphillip@gmail.com>
2023-03-31 11:04:15 -0400

    dev container updates

--- postCreateScript.sh
+++ postCreateScript.sh
@@ -6,4 +6,6 @@ go get -u github.com/mdempsky/gocode
 go get golang.org/x/tools/cmd/goimports
 go get -u golang.org/x/lint/golint
 go get golang.org/x/tools/gopls
+go install -v github.com/ramya-rao-a/go-outline@v0.0.0-20210608161538-9736a4bde949
+go install -v golang.org/x/tools/gopls@latest
 go mod tidy
---
+++ postStartScript.sh
@@ -0,0 +1,2 @@
+go build -o panurge
+./panurge
